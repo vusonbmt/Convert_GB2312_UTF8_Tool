534aa5289691978647060814aa597903b29e4e7b
SC <vusonbmt@gmail.com>
2023-03-18 21:14:22 +0700

    Update check valid and add exception
    Signed-off-by: SC <vusonbmt@gmail.com>

--- convert2utf8.py
+++ convert2utf8.py
@@ -15,20 +15,35 @@ def convert_encoding(file_path, input_encoding, output_encoding):
     with codecs.open(file_path, 'w', encoding=output_encoding) as file:
         file.write(content)
 
+def is_valid_encoding(file_path, output_encoding):
+    try:
+        with codecs.open(file_path, 'r', encoding=output_encoding) as file:
+            file.read()
+        return True
+    except UnicodeDecodeError:
+        print(f"Error: {file_path} contains illegal multibyte sequence.")
+        return False
+    
 def convert_files_encoding(folder_path, extensions, input_encoding, output_encoding):
     files = []
     for extension in extensions:
         files += get_files_by_extension(folder_path, extension)
 
     for file in files:
-        # Get the directory and name of the file
-        file_dir, file_name = os.path.split(file)
-        # Set the full path of the new file with the same extension
-        new_file_path = os.path.join(file_dir, file_name)
-        # Convert the encoding of the file and save it with the same name
-        convert_encoding(file, input_encoding, output_encoding)
-        os.rename(file, new_file_path)
-
+        try:
+            # Get the directory and name of the file
+            file_dir, file_name = os.path.split(file)
+            # Set the full path of the new file with the same extension
+            new_file_path = os.path.join(file_dir, file_name)
+            # Check if the file can be encoded with the output encoding 
+            if is_valid_encoding(file, output_encoding):
+                # Convert the encoding of the file and save it with the same name
+                convert_encoding(file, input_encoding, output_encoding)
+                os.rename(file, new_file_path)
+            else:
+                print(f"Skipping file: {file}")            
+        except Exception as e:
+            print(f"Error converting {file}: {e}")            
         
 def main():
     if len(sys.argv) < 2:
@@ -44,7 +59,7 @@ def main():
     input_encoding = 'gb2312'
     output_encoding = 'utf-8'
     extensions = ['.c', '.h']
-# Debug
+
     for extension in extensions:
         files = get_files_by_extension(folder_path, extension)
         print(f"Files with extension '{extension}':")
